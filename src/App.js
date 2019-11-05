4f3dd9cbaee70f45c906f1c9154c3205f60d693e
Yoonwonwoo <51125312+Yoonwonwoo@users.noreply.github.com>
2019-11-05 21:17:04 +0900

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React, { Component } from 'react'
-
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-class App extends Component {
-  render(){
-    return (
-      <>
-         <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-      </>
-    )
-  }
+function App(){
+  return ( 
+    <div className="root">
+      <header>header</header>
+      <nav>nav</nav>
+      <div className="div1">
+        <section>main</section>
+        <aside>aside</aside>
+      </div>
+      <footer>footer</footer>
+    </div>  
+    );
 }
 
 
